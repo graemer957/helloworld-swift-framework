877471de3d15739305473e9ebf61dffaea77284a
Graeme Read <graeme@sigma957.net>
2017-01-02 16:57:22 +0000

    Add demo functionality to framework

---
+++ HelloWorld/HelloWorld.swift
@@ -0,0 +1,18 @@
+//
+//  HelloWorld.swift
+//  HelloWorld
+//
+//  Created by Graeme Read on 02/01/2017.
+//  Copyright © 2017 Graeme Read. All rights reserved.
+//
+
+import Foundation
+
+
+public struct HelloWorld {
+    
+    public static func sayIt() {
+        print("Hello World!")
+    }
+    
+}
