548af148588609f074038b2d7cfb91e65ad3f34f
Graeme Read <graeme@sigma957.net>
2017-03-11 20:52:02 +0000

    Add demo target for exercising the framework during development

---
+++ Demo/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Demo
+//
+//  Created by Graeme Read on 11/03/2017.
+//  Copyright © 2017 Graeme Read. All rights reserved.
+//
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - Properties
+    var window: UIWindow?
+    
+    
+    // MARK: - UIApplicationDelegate
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
---
+++ Demo/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Graeme Read on 11/03/2017.
+//  Copyright © 2017 Graeme Read. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+}
